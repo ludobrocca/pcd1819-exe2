e0eaa8f7d9f3f5afb4e17ee99db7fb952de4f7e6
ludobrocca <lbrocca29@gmail.com>
2019-01-02 19:03:30 +0100

    new proposed solution

--- src/main/java/pcd2018/exe2/DiffieHellman.java
+++ src/main/java/pcd2018/exe2/DiffieHellman.java
@@ -33,53 +33,59 @@ public class DiffieHellman {
    * @return tutte le coppie di possibili segreti a,b
    */
   public List<Integer> crack(long publicA, long publicB) {
-    List<Integer> res = new ArrayList<Integer>();
+    List<Integer> res = new ArrayList<>();
 
 
-
+      /*
+      I am creating 2 HashMaps: each one contains a value between 0 and LIMIT
+      mapped to the result of ModPow()
+       */
       HashMap<Integer, Long> Ahashmap = new HashMap<>(
-              IntStream.rangeClosed(0, LIMIT)
-                      .parallel()
+              Collections.synchronizedMap(IntStream.rangeClosed(0, LIMIT)
                       .boxed()
                       .collect(
                               Collectors.toMap(x-> x, x-> DiffieHellmanUtils.modPow(publicB, x, p))
-                      )
+                      ))
       );
 
 
 
       HashMap<Integer,Long> Bhasmap = new HashMap<>(
-              IntStream.rangeClosed(0,LIMIT)
-                      .parallel()
+              Collections.synchronizedMap(IntStream.rangeClosed(0,LIMIT)
                       .boxed()
                       .collect(
                                 Collectors.toMap(x-> x, x-> DiffieHellmanUtils.modPow(publicA,x,p))
-              )
+              ))
 
 
       );
 
       Ahashmap.entrySet().parallelStream().forEach(x-> {
 
-          Integer[] values=Bhasmap.entrySet()
+          Integer[] intValues=Bhasmap.entrySet()
                   .parallelStream()
                   .filter(z -> z.getValue().equals(x.getValue()))
                   .map(Map.Entry::getKey)
                   .toArray(Integer[]::new);
 
-          Arrays.stream(values).parallel().forEach(el -> synchroAdd(res,x.getKey(),el));
+          Arrays.stream(intValues).parallel().forEach(el -> DiffieHellman.synchroAdd(res,x.getKey(),el));
 
       });
 
     return res;
   }
 
-
-    public synchronized void synchroAdd(List<Integer> res,Integer a,Integer b) {
+  /**
+   * Utility private method that adds two elements to a List,
+   * the synchronized keyword guarantees that the operation is Thread-safe
+   * @param res List that needs to add elemnts
+   * @param a first element
+   * @param b second element
+   */
+    private static synchronized void synchroAdd(List res, Integer a,Integer b) {
       res.add(a);
       res.add(b);
     }
 
-
-
 }
+
